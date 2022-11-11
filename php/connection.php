a0563cbde120c0fd4b97713a94d3458500593108
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-10 21:13:14 -0300

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -7,10 +7,10 @@
     $conn = mysqli_connect($hostname, $username, $password, $database);
 
     if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+        die("Erro de conexão: " . mysqli_connect_error());
     }
 
-    echo "Connected successfully.";
+    echo "Conectado com sucesso.";
 
     mysqli_close($conn);
 ?>
