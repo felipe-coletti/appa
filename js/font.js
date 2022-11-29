75c2916e50f41a9a0f5ddfd812f417b83feced0b
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-11-29 17:27:16 -0300

    Update font.js

--- js/font.js
+++ js/font.js
@@ -2,21 +2,21 @@ $(document).ready(function(){
     let titleSize = 20
     let textSize = 16
 
-    $('#increase').click(function(){
+    $("#increase").click(function(){
         if(titleSize < 28){
-            $('#text h1').css({'font-size': ++titleSize + 'px'})
+            $("#text h1").css({"font-size": ++titleSize + "px"})
         }
         if(textSize < 24){
-            $('body').css({'font-size': ++textSize + 'px'})
+            $("body").css({"font-size": ++textSize + "px"})
         }
     })
     
-    $('#decrease').click(function(){
+    $("#decrease").click(function(){
         if(titleSize > 12){
-            $('#text h1').css({'font-size': --titleSize + 'px'})
+            $("#text h1").css({"font-size": --titleSize + "px"})
         }
         if(textSize > 8){
-            $('body').css({'font-size': --textSize + 'px'})
+            $("body").css({"font-size": --textSize + "px"})
         }
     })
 })
